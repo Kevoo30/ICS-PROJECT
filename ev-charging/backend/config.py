a9b0a08a64f8057a218834316c6778d03d1f1cb8
Wayne Kimutai <96531439+Virtuoso1@users.noreply.github.com>
2026-06-24 10:38:58 +0300

    updated violations and queue

--- ev-charging/backend/config.py
+++ ev-charging/backend/config.py
@@ -9,5 +9,5 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     FIREBASE_CREDENTIALS = os.getenv("FIREBASE_CREDENTIALS")
     VIOLATION_LIMIT = int(os.getenv("VIOLATION_LIMIT", 3))
-    NO_SHOW_DEADLINE_MINUTES = int(os.getenv("NO_SHOW_DEADLINE_MINUTES", 10))
+    NO_SHOW_DEADLINE_MINUTES = int(os.getenv("NO_SHOW_DEADLINE_MINUTES", 120))
     DELAY_LIMIT = int(os.getenv("DELAY_LIMIT", 3))
